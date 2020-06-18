98e9a062a879f9c170f55485e999424eeaa09b5f
esraaEmad <esso.em.96@gmail.com>
2020-06-18 06:07:36 +0200

    redux Files and install some libraries

--- src/App.js
+++ src/App.js
@@ -1,48 +1,39 @@
 import React, { Fragment } from 'react';
-import logo from './logo.svg';
-// import './App.css';
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
-import { createMuiTheme, CssBaseline, Container } from '@material-ui/core';
-import { ThemeProvider } from '@material-ui/styles';
-import { purple } from '@material-ui/core/colors';
-import Button from '@material-ui/core/Button';
-import Header from './components/header'; 
-import Footer from './components/footer';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-const theme = createMuiTheme({
-    palette: {
-        primary: {
-            // Purple and green play nicely together.
-            main: '#A64AC9',
-            light: '#da7bfd',
-            dark: '#741498',
-        },
-        secondary: {
-            // This is green.A700 as hex.
-            main: '#FCCD04',
-            light: '#ffff52',
-            dark: '#c49d00',
-        },
-        type: 'light',
-    },
-});
+
+//Redux 
+import { Provider } from 'react-redux';
+import store from './store';
+
+//Components 
+import Authentication from './components/Authentication/Authentication';
+
+
+
 
 function App() {
     return (
-        <Fragment>
-            <CssBaseline />
-            <ThemeProvider theme={theme}>
-                <Header />
-                <Container maxWidth="lg">
-                {/* <Switch>
+        <Provider store={store}>
+            
+        <Router>
 
-                </Switch> */}
-                </Container>
-                <Footer/>
-            </ThemeProvider>
+        <Fragment>
+            <Switch>
+
+             <Route path="/Auth" component={Authentication}/>
+
+
+            </Switch>
         </Fragment>
+
+        </Router>
+
+        </Provider>
     );
 }
 
--- src/components/Authentication.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-class Authentication extends Component {
-    state = {  }
-    render() { 
-        return ( <h1>Test</h1> );
-    }
-}
- 
-export default Authentication;
